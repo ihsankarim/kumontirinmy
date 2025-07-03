41f720ded730aeb915d9dad6c8bb97b4fa44fdc0
ihsankarim <ihsanalkarim305@gmail.com>
2025-07-03 16:15:50 +0700

    add: profile information

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,6 +1,14 @@
-import { requireRole } from "@/lib/auth";
+"use client"
+import { useSession } from "next-auth/react";
 import React from "react";
 
 export default function ProfilePage() {
-  return <div>Profile Page</div>;
+  const { data: session } = useSession();
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Profile</h1>
+      <p>Email {session?.user.email}</p>
+      <p>Role {session?.user.role}</p>
+    </div>
+  );
 }
